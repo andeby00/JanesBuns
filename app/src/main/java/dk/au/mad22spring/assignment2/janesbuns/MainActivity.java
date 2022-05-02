b962d4f6a7de7008f92ead0294068073c459229a
Rasmus <rasmuswium@hotmail.com>
2022-05-02 18:36:46 +0200

    Initial commit

---
+++ app/src/main/java/dk/au/mad22spring/assignment2/janesbuns/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.au.mad22spring.assignment2.janesbuns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
