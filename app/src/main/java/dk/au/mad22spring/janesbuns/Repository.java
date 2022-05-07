8516a2488d9f49ec764014e24ed9cd49012ccc03
Mads Morratz <mnm@speciallabs.com>
2022-05-07 22:39:13 +0200

    sexmeddineks

--- app/src/main/java/dk/au/mad22spring/janesbuns/Repository.java
+++ app/src/main/java/dk/au/mad22spring/janesbuns/Repository.java
@@ -6,18 +6,27 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import dk.au.mad22spring.janesbuns.models.CreamBun;
+import dk.au.mad22spring.janesbuns.models.Currency;
 import dk.au.mad22spring.janesbuns.models.User;
 
 public class Repository {
@@ -119,5 +128,26 @@ public class Repository {
         }
 
         cart.setValue(tempList);
+    }
+
+    public void requestCurrency(MutableLiveData<List<Currency>> currencyList) {
+        StringRequest mRequest = new StringRequest(
+                Request.Method.GET,
+                "https://free.currconv.com/api/v7/currencies?apiKey=8c339bcbe21bcd7cbb1b",
+                response -> handleResponse(response, currencyList),
+                error -> Log.e(TAG, "that did not work", error )
+        );
+        queue.add(mRequest);
+    }
+
+    private void handleResponse(String response, MutableLiveData<List<Currency>> currencyList) {
+        List<Currency> tempCurrency = parseJson(response);
+        currencyList.setValue(tempCurrency);
+    }
+
+    public static List<Currency> parseJson(String json){
+        Gson gson = new GsonBuilder().create();
+        Currency currency = gson.fromJson(json, Currency.class);
+        return currency.currencies;
     }
 }
---
+++ app/src/main/java/dk/au/mad22spring/janesbuns/utils/CurrencyAPI.java
@@ -0,0 +1,59 @@
+package dk.au.mad22spring.janesbuns.utils;
+
+import android.app.Application;
+import android.util.Log;
+
+
+import androidx.annotation.NonNull;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import dk.au.mad22spring.janesbuns.Repository;
+import dk.au.mad22spring.janesbuns.models.Currency;
+
+    // https://www.currencyconverterapi.com/docs
+public class CurrencyAPI {
+    private static final String TAG = "CurrencyAPI";
+
+
+        // https://free.currconv.com/api/v7/convert?q=DKK_EUR,EUR_DKK&compact=ultra&apiKey=8c339bcbe21bcd7cbb1b
+    private static void sendRequest(String convertTo, RequestQueue queue, Application app) {
+        StringRequest mRequest = new StringRequest(
+                Request.Method.GET,
+                "https://free.currconv.com/api/v7/convert?q=DKK_" + convertTo + "," + convertTo + "_DKK&compact=ultra&apiKey=8c339bcbe21bcd7cbb1b",
+                response -> getCurrency(response, app),
+                error -> Log.e(TAG, "that did not work", error ));
+        queue.add(mRequest);
+    }
+
+    private static void getCurrency(String response, Application app) {
+        Log.d(TAG, "setCurrency: " + response);
+        Executors.newSingleThreadExecutor().execute(() -> {
+            List<Currency> currency = parseJson(response);
+            if (currency != null){
+                Log.d(TAG, "saveDrink: " + currency.get(0).getCurrencySymbol());
+            }
+        });
+    }
+
+        public static List<Currency> parseJson(String json){
+            Gson gson = new GsonBuilder().create();
+            Currency currency = gson.fromJson(json, Currency.class);
+            return currency.currencies;
+        }
+}
